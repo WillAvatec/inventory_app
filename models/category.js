6dc9e64fd71c8f8f61a966a639bfc7d82b4c2b18
Emanuel <115133403+WillAvatec@users.noreply.github.com>
2023-09-21 14:43:53 -0500

    feat: new model for categories

---
+++ models/category.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const CategorySchema = new Schema({
+  name: {
+    type: String,
+    maxLength: 30,
+    required: true,
+  },
+  description: {
+    type: String,
+  },
+});
+
+// Virtual to get URL
+CategorySchema.virtual("url").get(function () {
+  return `/catalog/category/${this._id}`;
+});
+
+module.exports = mongoose.model("Category", CategorySchema);
