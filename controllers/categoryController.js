4ad8389526bcbb7d241f379dd8110232f688fb0f
Emanuel <115133403+WillAvatec@users.noreply.github.com>
2023-09-21 21:56:16 -0500

    chore: Add form to post a new category

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -30,3 +30,65 @@ exports.category_detail = asyncHand(async (req, res, next) => {
     category,
   });
 });
+
+// GET: Display form to create new category
+exports.category_create_get = asyncHand(async (req, res) => {
+  res.render("category_form", {
+    title: "Create a new category",
+  });
+});
+
+// POST: Handle submitted data and sanitize
+exports.category_create_post = [
+  // Validate and sanitize fields.
+  body("name")
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage("A name must be specified.")
+    .isAlpha()
+    .withMessage("Name must be alphabetic letters only."),
+  body("description")
+    .trim()
+    .isLength({ max: 200 })
+    .escape()
+    .withMessage("Description can only be up to 200 characters"),
+
+  // Process request after validation and sanitization.
+  asyncHand(async (req, res, next) => {
+    // Extract the errors from the information
+    const errors = validationResult(req);
+
+    //Destructure variables from request body
+    const { name, description } = req.body;
+
+    const newCategory = new Category({
+      name,
+      description,
+    });
+
+    if (!errors.isEmpty()) {
+      //Found errors, return form with sanitized values
+      res.render("category_form", {
+        title: "Create a new category",
+        category: newCategory,
+        errors: errors.array(),
+      });
+      // TODO: check if return is necessary or not
+      return;
+    } else {
+      //Check if category already exists first
+      const categoryExists = Category.findOne({ name })
+        .collation({ locale: "en", strength: 2 })
+        .exec();
+
+      if (categoryExists) {
+        // Redirect to the detail page of the category
+        res.redirect(categoryExists.url);
+      } else {
+        await newCategory.save();
+        res.redirect(newCategory.url);
+      }
+    }
+  }),
+];
