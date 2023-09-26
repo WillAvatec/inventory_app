25f1ecf68d9e67ab60925f4c6b269ea16b8c024d
Emanuel <115133403+WillAvatec@users.noreply.github.com>
2023-09-25 19:31:26 -0500

    chore: Add catalog router to middleware chain

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 
 var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
+const catalogRouter = require("./routes/catalog");
 
 var app = express();
 
@@ -20,6 +20,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
+app.use("/catalog", catalog);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
