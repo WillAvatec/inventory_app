07a362dd11eea93c226e4e676eed542386fc094b
Emanuel <115133403+WillAvatec@users.noreply.github.com>
2023-10-09 15:25:41 -0500

    feat: Activate timestamps to add creation date

--- models/item.js
+++ models/item.js
@@ -1,34 +1,37 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const ItemSchema = new Schema({
-  name: {
-    type: String,
-    required: true,
-  },
-  description: {
-    type: String,
-  },
-  price: {
-    type: Number,
-    required: true,
-  },
-  category: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: "Category",
+const ItemSchema = new Schema(
+  {
+    name: {
+      type: String,
       required: true,
     },
-  ],
-  stock: {
-    type: Number,
-    required: true,
-    default: 0,
+    description: {
+      type: String,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    category: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Category",
+        required: true,
+      },
+    ],
+    stock: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    image: {
+      type: String,
+    },
   },
-  image: {
-    type: String,
-  },
-});
+  { timestamps: true }
+);
 
 // Virtual to get URl
 
